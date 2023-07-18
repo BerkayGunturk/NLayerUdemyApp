930ea8cec3d7ca5ce5ee20d602685df143d23931
Berkay Güntürk <78702311+BerkayGunturk@users.noreply.github.com>
2023-07-18 23:34:17 +0300

    Products Controller Update

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -50,9 +50,35 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
             return View();
 
-
         }
 
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await _services.GetByIdAsync(id);
+            var categories = await _categoryService.GetAllAsync(); //tüm kategori getir.
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList()); //dto çevirdim
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", product.CategoryId);
+            return View(_mapper.Map<ProductDto>(product));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _services.UpdateAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            var categories = await _categoryService.GetAllAsync(); //tüm kategori getir.
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList()); //dto çevirdim
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
+            return View(productDto);
+        }
 
 
     }
